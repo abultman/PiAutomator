26de04afc530395fe318c19621819fc7460a7275
Bultje76 <arne@bultje.com>
2013-12-05 21:54:38 +0100

    remove debug println

--- prg/automate.py
+++ prg/automate.py
@@ -9,11 +9,7 @@ import receivers
 import inputs
 import schedule
 import rules
-import sys
 from config import AutomationConfig
-
-sys.path.append('/some/dir')
-print sys.path
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(message)s')
 basedir = os.path.normpath("%s/.." % (os.path.dirname(os.path.abspath(__file__))))
