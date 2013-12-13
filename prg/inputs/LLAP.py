785aef7054ce394698847cd5c033543eee5a6bf7
Bultje76 <arne@bultje.com>
2013-12-13 22:27:38 +0100

    Clean up logging

--- prg/inputs/LLAP.py
+++ prg/inputs/LLAP.py
@@ -24,7 +24,7 @@ llap_commands = {
     'BATT': ['batterylevel', float],
     'STARTED': ['lowbattery', _false],
     'LVAL': ['lightlevel', float],
-    'TEMP': ['temperature', float],
+        'TEMP': ['temperature', float],
     'TMPA': ['temperature', float],
     'ANA': ['analog', int],
     'BUTTON': ['button', str]
@@ -66,17 +66,19 @@ class LLAPDaemon(object):
 
     def receive(self):
         __logger__.info("Starting in receiving mode for llap")
-        while True:
-            n = self.ser.inWaiting()
-            if (n > 0):
-                self.current_buffer += self.ser.read(n)
-                self.find_messages()
+        try:
+            while True:
+                n = self.ser.inWaiting()
+                if (n > 0):
+                    self.current_buffer += self.ser.read(n)
+                    self.find_messages()
+        except:
+            pass
 
     def find_messages(self):
         try:
             self.current_buffer = ''.join(dropwhile(lambda x: not x == 'a', (i for i in self.current_buffer)))
             if len(self.current_buffer) >= 12:  # 12 is the LLAP message length
-                 __logger__.info("current buffer: %s", self.current_buffer)
                  if self.p.match(self.current_buffer):
                      self.process_device_message(self.current_buffer[0:12])
                      self.current_buffer = self.current_buffer[12:]
@@ -88,7 +90,6 @@ class LLAPDaemon(object):
             __logger__.warn("exception happened")
 
     def process_device_message(self, message):
-        __logger__.info("message %s", message)
         device = message[1:3]
         command = message[3:].replace('-','')
         if command == 'HELLO':
