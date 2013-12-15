1f0185aabad602fea8fbcc9ec1e873f1a929e4e1
Bultje76 <arne@bultje.com>
2013-12-15 10:13:25 +0100

    Make sure our own python files are first on the path

--- bin/start-automator.py
+++ bin/start-automator.py
@@ -4,7 +4,9 @@ import sys
 
 basedir = os.path.normpath("%s/.." % (os.path.dirname(os.path.abspath(__file__))))
 
+sys.path.reverse()
 sys.path.append("%s/prg" % basedir)
 sys.path.append("%s/lib" % basedir)
+sys.path.reverse()
 
 import automate
