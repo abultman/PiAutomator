5851eb2f84fd7f2050dfec30a751c9aab900335e
Bultje76 <arne@bultje.com>
2013-12-29 17:13:35 +0100

    Add 'raw' mode to pilight receiver. Records raw json messages so you can respond to those without mapping it

--- prg/inputs/PiLight.py
+++ prg/inputs/PiLight.py
@@ -5,6 +5,8 @@ import socket
 import json
 import logging
 import threading
+import time
+from config import LocalSettings
 from inputs import AnInput
 
 pilight_sensors = {}
@@ -20,9 +22,6 @@ def init(config):
     receiver = PiLightDaemon(
         config.getSetting(['pilight', 'host'], '127.0.0.1'),
         config.getSetting(['pilight', 'port'], 5000))
-    pilightthread = threading.Thread(target=receiver.receive, name='pilight-receiver')
-    pilightthread.daemon = True
-    pilightthread.start()
 
 class PiLight(AnInput):
     def __init__(self,  name, context, settings):
@@ -33,57 +32,137 @@ class PiLight(AnInput):
         self.values = None
         pilight_sensors["%s.%s" %(self.room, self.input)] = self
 
-    def update(self, data, publish_key = None):
-        if not self.started: return
-        origvalues = data['values']
+    def __get_values__(self, origvalues):
         values = {}
         for key in origvalues:
-            if origvalues[key] in ['on', 'up']:values[key+'_int'] = 1
-            elif origvalues[key] in ['off', 'down']:values[key+'_int'] = 0
+            if origvalues[key] in ['on', 'up']:
+                values[key + '_int'] = 1
+            elif origvalues[key] in ['off', 'down']:
+                values[key + '_int'] = 0
             values[key] = origvalues[key]
-        if publish_key:
-            self.publish(values, 'pilight.%s' % (publish_key))
-        elif self.scale:
-            result = {}
-            for key in values:
-                result[key] = float(values[key]) * self.scale
-            self.publish(result)
+        return values
+
+    def update(self, data, publish_key = None):
+        if not self.started: return
+        if publish_key == 'raw':
+            self.update_raw(data)
         else:
-            self.publish(values)
+            origvalues = data['values']
+            values = self.__get_values__(origvalues)
+            if publish_key:
+                self.publish(values, 'pilight.%s' % (publish_key))
+            elif self.scale:
+                result = {}
+                for key in values:
+                    result[key] = float(values[key]) * self.scale
+                self.publish(result)
+            else:
+                self.publish(values)
+
+    def update_raw(self, data):
+        # {"code":{"id":11449862,"unit":0,"state":"up"},"origin":"receiver","protocol":"archtech_screens","repeats":2}
+        if 'protocol' in data and 'code' in data:
+            wrapped_data = LocalSettings(data['code'])
+            protocol = data['protocol']
+            id = wrapped_data.getsetting('id', 'noid')
+            unit = wrapped_data.getsetting('unit', 'nounit')
+            publish_key = 'pilight.raw.%s.%s.%s' % (protocol, id, unit)
+            values = self.__get_values__(data['code'])
+            self.publish(values, publish_key)
 
 class PiLightDaemon(object):
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.open()
         self.current_buffer = ""
+        self.current_buffer_raw = ""
+        pilightthread = threading.Thread(target=self.receive, name='pilight-receiver')
+        pilightthread.daemon = True
+        pilightthread.start()
+        pilightthread = threading.Thread(target=self.receive_raw, name='pilight-receiver-raw')
+        pilightthread.daemon = True
+        pilightthread.start()
 
-    def open(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.host, self.port))
-        self.socket.send(json.dumps({'message': 'client gui'}))
-        __logger__.info("connected to pilight as a receiver")
-        self.socket.send(json.dumps({'message': 'request config'}))
-        self.socket.recv
+    def connect_raw(self):
+        try:
+            self.socket_raw = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket_raw.connect((self.host, self.port))
+            self.socket_raw.send(json.dumps({'message': 'client receiver'}))
+            __logger__.info("Starting in receiving mode for pilight raw")
+        except:
+            self.socket_raw = None
+
+    def connect_gui(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.host, self.port))
+            self.socket.send(json.dumps({'message': 'client gui'}))
+            self.socket.send(json.dumps({'message': 'request config'}))
+            __logger__.info("Starting in receiving mode for pilight")
+        except:
+            self.socket = None
+
+    def receive_raw(self):
+        def close():
+            try:
+                self.socket_raw.close()
+            except:
+                pass
+            self.socket_raw = None
+            __logger__.info("raw input terminated, pilight down?")
+
+        while True:
+            self.connect_raw()
+            while self.socket_raw is not None:
+                try:
+                    msg = self.socket_raw.recv(1024)
+                    if len(msg) == 0:
+                        close()
+                    else:
+                        self.current_buffer_raw = "%s%s" %(self.current_buffer_raw, msg)
+                        self.current_buffer_raw = self.find_messages(self.current_buffer_raw)
+                except:
+                    close()
+
+            time.sleep(2)
 
     def receive(self):
-        __logger__.info("Starting in receiving mode for pilight")
-        while True:
-            msg = self.socket.recv(1024)
-            self.current_buffer = "%s%s" %(self.current_buffer, msg)
-            self.find_messages()
+        def close():
+            try:
+                self.socket.close()
+            except:
+                pass
+            self.socket = None
+            __logger__.info("raw input terminated, pilight down?")
 
-    def find_messages(self):
+        while True:
+            self.connect_gui()
+            while self.socket is not None:
+                try:
+                    msg = self.socket.recv(1024)
+                    if len(msg) == 0:
+                        close()
+                    else:
+                        self.current_buffer = "%s%s" %(self.current_buffer, msg)
+                        self.current_buffer = self.find_messages(self.current_buffer)
+                except:
+                    close()
+            time.sleep(2)
+
+
+    def find_messages(self, buffer):
         try:
             new_buffer = ""
-            for message in self.current_buffer.splitlines(True):
+            for message in buffer.splitlines(True):
                 if message.endswith('\n'):
                     self.process_message(message)
                 else:
                     new_buffer = message
-            self.current_buffer = new_buffer
-        except:
+            return new_buffer
+        except Exception, e:
             __logger__.warn("exception happened")
+            __logger__.exception(e)
+            return buffer
 
     def process_device_message(self, message):
         devices = message['devices']
@@ -97,16 +176,22 @@ class PiLightDaemon(object):
                     pilight_sensors['all.all'].update(message, key)
 
 
+    def process_raw_message(self, message):
+        if 'all.all' in pilight_sensors:
+            pilight_sensors['all.all'].update(message, 'raw')
+
+
     def process_config_message(self, message):
         config = message['config']
         __logger__.info("Configuration of pilight\n%s", config)
-
 
     def process_message(self, messagestr):
         __logger__.debug(messagestr)
         message = json.loads(messagestr)
         if 'devices' in message:
             self.process_device_message(message)
-        if 'config' in message:
+        elif 'config' in message:
             self.process_config_message(message)
+        else:
+            self.process_raw_message(message)
 
