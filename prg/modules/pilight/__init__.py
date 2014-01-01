725480142c8e6363703a102021d1f92f039c9d84
Bultje76 <arne@bultje.com>
2014-01-01 20:21:41 +0100

    removed blank line

--- prg/modules/pilight/__init__.py
+++ prg/modules/pilight/__init__.py
@@ -2,7 +2,6 @@ from core import SCOPE_INPUT, SCOPE_RECEIVER
 from modules.pilight.pilight_input import pilight_input, init_module as input_init
 from modules.pilight.pilight_output import pilight_output
 
-
 def init_module(config, scope):
     if scope == SCOPE_INPUT:
         input_init(config)
