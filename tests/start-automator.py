e6e6dd276d7cc6c9d959948746de0b8aecd0e195
Bultje76 <arne@bultje.com>
2014-01-01 19:09:49 +0100

    allow starting with stubs

---
+++ tests/start-automator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import os
+import sys
+
+basedir = os.path.normpath("%s/.." % (os.path.dirname(os.path.abspath(__file__))))
+
+sys.path.reverse()
+sys.path.append("%s/prg" % basedir)
+sys.path.append("%s/stubs" % basedir)
+sys.path.reverse()
+
+import automate
