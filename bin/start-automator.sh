e90db895066868c13b23f227f644f8644f244c3d
Bultje76 <arne@bultje.com>
2014-12-10 14:14:10 +0100

    Make sure the virtual env is actually used

--- bin/start-automator.sh
+++ bin/start-automator.sh
@@ -2,4 +2,4 @@ DIR=$( cd "$( dirname $0 )" && pwd )
 echo $DIR
 source $DIR/../piautomatorenv/bin/activate
 pip install -r $DIR/../conf/requirements.txt
-$DIR/start-automator.py
+python $DIR/start-automator.py
