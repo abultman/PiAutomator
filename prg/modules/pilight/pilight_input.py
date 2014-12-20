035fd04919cc305e9b9321bdd986d58bcd9cf247
Bultje76 <arne@bultje.com>
2014-12-20 08:40:48 +0100

    Ehm..

--- prg/modules/pilight/pilight_input.py
+++ prg/modules/pilight/pilight_input.py
@@ -200,8 +200,7 @@ class pilightDaemon(object):
     def process_message(self, messagestr):
         try:
             if messagestr == '\n': return
-            message = json.loads(messagestr)
-            message = message[message.index('{'):message.rindex('}') + 1]
+            message = json.loads(messagestr[messagestr.index('{'):messagestr.rindex('}') + 1])
             if 'origin' in message and message['origin'] == 'config':
                 if 'devices' in message:
                     self.process_device_message(message)
